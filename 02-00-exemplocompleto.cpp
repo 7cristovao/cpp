5f895c91a1e084f93a0e63e5d6c7085f8bc2bbd7
7cristovao <38305965+7cristovao@users.noreply.github.com>
2022-04-28 14:30:04 -0300

    Add files via upload

---
+++ 02-00-exemplocompleto.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char *argv[])
+{
+    cout << "Linguagem C++\n";
+    system("read -p 'Press Enter to continue...' var");
+    return 0;
+}
---
+++ 02-04-b-funcoes.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char *argv[])
+{
+
+//  x = 9 % 2 + 9 / 2
+    system("read -p 'x = 9 % 2 + 9 / 2' var");
+ 
+    double x;
+    x = 9 % 2 + 9 / 2;
+    cout << "x = " << x;
+    cout << "\n";
+    system("read -p 'Pressione qualquer tecla para continuar...' var");
+    return 0;
+}
