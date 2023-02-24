f303fbd180d9218dbb8d04e55013537be7ad95df
7cristovao <38305965+7cristovao@users.noreply.github.com>
2023-02-24 19:51:03 -0300

    Update 02-00-exemplocompleto.cpp

--- exercicios/capitulo02/02-00-exemplocompleto.cpp
+++ exercicios/capitulo02/02-00-exemplocompleto.cpp
@@ -1,3 +1,5 @@
+// Este exemplo exibe a palavra linguagem C++ na primeira linha do vídeo. O caractere de controle \n move o cursor para segunda linha do vídeo
+
 #include <iostream>
 using namespace std;
 int main(int argc, char *argv[])
